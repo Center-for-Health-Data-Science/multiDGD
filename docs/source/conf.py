a6497119587bc2da383a899fa674f0367e3153e1
viktoriaschuster <viki.schuster@web.de>
2024-09-19 15:33:54 +0200

    docs: try change config path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,12 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+#sys.path.insert(0, os.path.abspath('../..'))
+
+# Get the absolute path to the package.
+path = os.path.abspath("../../src/multiDGD")
+
+# Insert the path into the PATH.
+sys.path.insert(0, path)
 
 # Configuration file for the Sphinx documentation builder.
 #
