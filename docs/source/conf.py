5dba6abb5d3feb080efdfca655f1106ebaffcc95
viktoriaschuster <viki.schuster@web.de>
2024-09-19 15:43:31 +0200

    docs: try a different source path in config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import sys
 #sys.path.insert(0, os.path.abspath('../..'))
 
 # Get the absolute path to the package.
-path = os.path.abspath("../../src/multiDGD")
+path = os.path.abspath("../../src")
 
 # Insert the path into the PATH.
 sys.path.insert(0, path)
